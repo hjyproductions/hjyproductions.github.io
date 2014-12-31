55171679084477c39c74b552c712dede00654abe
vernacular <d.lin10987@gmail.com>
2014-12-30 19:38:46 -0800

    try1

---
+++ script.js
@@ -0,0 +1,96 @@
+
+$(function Call() {
+	$('#about1').hide();
+	$('#about').on('click', function(){
+		$('#about1').fadeToggle(400);
+	});
+});
+
+$(function() {
+	$('#contact').hide();
+	$('#contactbutton').on('click', function(){
+		$('#contact').fadeToggle(400);
+	});
+});
+
+$(function() {
+	var btn = $('#IILM');
+	btn.on('click', function () {
+	    var tv_main_channel = $('#tv_main_channel');
+	    tv_main_channel.attr('src', 'vids/loseMyself.mp4');
+	    var video_block = $('#bgvid');
+	    video_block.load();
+	});
+});
+$(function() {
+	var btn = $('#300VO');
+	btn.on('click', function () {
+	    var tv_main_channel = $('#tv_main_channel');
+	    tv_main_channel.attr('src', 'vids/300_violin.mp4');
+	    var video_block = $('#bgvid');
+	    video_block.load();
+	});
+});
+$(function() {
+	var btn = $('#Crime');
+	btn.on('click', function () {
+	    var tv_main_channel = $('#tv_main_channel');
+	    tv_main_channel.attr('src', 'vids/crime.mp4');
+	    var video_block = $('#bgvid');
+	    video_block.load();
+	});
+});
+$(function() {
+	var btn = $('#CB');
+	btn.on('click', function () {
+	    var tv_main_channel = $('#tv_main_channel');
+	    tv_main_channel.attr('src', 'vids/chill_beat.mp4');
+	    var video_block = $('#bgvid');
+	    video_block.load();
+	});
+});
+$(function() {
+	var btn = $('#persevere');
+	btn.on('click', function () {
+	    var tv_main_channel = $('#tv_main_channel');
+	    tv_main_channel.attr('src', 'vids/persevere.mp4');
+	    var video_block = $('#bgvid');
+	    video_block.load();
+	});
+});
+$(function() {
+	var btn = $('#hope');
+	btn.on('click', function () {
+	    var tv_main_channel = $('#tv_main_channel');
+	    tv_main_channel.attr('src', 'vids/hope.mp4');
+	    var video_block = $('#bgvid');
+	    video_block.load();
+	});
+});
+$(function() {
+	var btn = $('#reminisce');
+	btn.on('click', function () {
+	    var tv_main_channel = $('#tv_main_channel');
+	    tv_main_channel.attr('src', 'vids/reminisce.mp4');
+	    var video_block = $('#bgvid');
+	    video_block.load();
+	});
+});
+$(function() {
+	var btn = $('#weightless');
+	btn.on('click', function () {
+	    var tv_main_channel = $('#tv_main_channel');
+	    tv_main_channel.attr('src', 'vids/weightless.mp4');
+	    var video_block = $('#bgvid');
+	    video_block.load();
+	});
+});
+$(function() {
+	var btn = $('#inspiration');
+	btn.on('click', function () {
+	    var tv_main_channel = $('#tv_main_channel');
+	    tv_main_channel.attr('src', 'vids/inspiration.mp4');
+	    var video_block = $('#bgvid');
+	    video_block.load();
+	});
+});
